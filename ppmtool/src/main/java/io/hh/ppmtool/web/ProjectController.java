722c3d33b3cb658e3ebc216ba57eea8e22d8805f
hhr-creator <hugo_heredia@outlook.com>
2020-07-13 15:22:31 -0500

    delete project operation

--- ppmtool/src/main/java/io/hh/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/hh/ppmtool/web/ProjectController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,4 +57,10 @@ public class ProjectController {
 		return projectService.findAllProjects();
 	}
 	
+	@DeleteMapping("/{projectId}")
+	public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+		projectService.deleteProjectByIdentifier(projectId);
+		
+		return new ResponseEntity<String>("Project with ID: '" + projectId + "' was deleted", HttpStatus.OK);
+	}
 }
