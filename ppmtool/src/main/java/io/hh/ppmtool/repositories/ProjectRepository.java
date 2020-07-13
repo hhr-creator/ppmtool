a16cf057afcb9297a40f8b5677a8e5fb379c6021
hhr-creator <hugo_heredia@outlook.com>
2020-07-13 13:44:19 -0500

    Project Controller and validations

---
+++ ppmtool/src/main/java/io/hh/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,14 @@
+package io.hh.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import io.hh.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> ids);
+
+}
---
+++ ppmtool/src/main/java/io/hh/ppmtool/services/ProjectService.java
@@ -0,0 +1,18 @@
+package io.hh.ppmtool.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.hh.ppmtool.domain.Project;
+import io.hh.ppmtool.repositories.ProjectRepository;
+
+@Service
+public class ProjectService {
+	
+	@Autowired
+	private ProjectRepository projectRepository;
+	
+	public Project saveOrUpdateProject(Project project) {
+		return projectRepository.save(project);
+	}
+}
