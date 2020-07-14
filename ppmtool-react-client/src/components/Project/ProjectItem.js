86db82c5723a2343db1ead59ccf88373248572ba
hhr-creator <hugo_heredia@outlook.com>
2020-07-14 15:52:12 -0500

    initial setup for react frontend

---
+++ ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ProjectItem extends Component {
+  render () {
+    return(
+      <h2>ProjectItem</h2>
+    )
+  }
+}
+
+export default ProjectItem;
