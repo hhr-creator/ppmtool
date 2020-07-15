0f2eb2c3761323f5694c00f704e8c470e1980f42
hhr-creator <hugo_heredia@outlook.com>
2020-07-15 14:44:51 -0500

    redux setup

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
 import Dashboard from './components/Dashboard';
+import AddProject from './components/Project/AddProject';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from "./store";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+
 }
 
 export default App;
