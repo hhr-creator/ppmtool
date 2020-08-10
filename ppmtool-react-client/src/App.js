c10f4a0d82ea5b2a929b402303ae6286acb7da28
hhr-creator <hugo_heredia@outlook.com>
2020-08-10 16:38:18 -0500

    project task front end

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -4,6 +4,10 @@ import Header from './components/Layout/Header';
 import Dashboard from './components/Dashboard';
 import AddProject from './components/Project/AddProject';
 import UpdateProject from './components/Project/UpdateProject';
+
+import ProjectBoard from './components/ProjectBoard/ProjectBoard';
+import AddProjectTask from './components/ProjectBoard/ProjectTasks/AddProjectTask';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -19,6 +23,8 @@ class App extends Component {
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
             <Route exact path="/updateProject/:id" component={UpdateProject} />
+            <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+            <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
           </div>
         </Router>
       </Provider>
