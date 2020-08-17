8bb8dee8e8c7418dbe5fb1a495072a044d60213e
hhr-creator <hugo_heredia@outlook.com>
2020-08-17 16:19:08 -0500

    register user front end

--- ppmtool/ppmtool-react-client/src/App.js
+++ ppmtool/ppmtool-react-client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
+import Landing from './components/Layout/Landing';
+import Register from './components/UserManagement/Register';
+import Login from './components/UserManagement/Login';
 import Dashboard from './components/Dashboard';
 import AddProject from './components/Project/AddProject';
 import UpdateProject from './components/Project/UpdateProject';
@@ -21,6 +24,15 @@ class App extends Component {
         <Router>
           <div className="App">
             <Header />
+            {
+              // Public routes
+            }
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            {
+              // Private Routes
+            }
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
             <Route exact path="/updateProject/:id" component={UpdateProject} />
