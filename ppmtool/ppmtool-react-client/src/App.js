7520c6d4d864587c1918c1bd69620be3ac17c2eb
hhr-creator <hugo_heredia@outlook.com>
2020-08-18 15:43:37 -0500

    securing front end

--- ppmtool/ppmtool-react-client/src/App.js
+++ ppmtool/ppmtool-react-client/src/App.js
@@ -17,6 +17,29 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from "./store";
 
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./securityUtils/setJWTToken";
+
+import { SET_CURRENT_USER } from './actions/types'
+
+const jwtToken = localStorage.jwtToken;
+
+if(jwtToken) {
+  setJWTToken(jwtToken);
+  const decoded_jwtToken = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_jwtToken
+  });
+
+  const currentTime = Date.now()/1000;
+
+  if(decoded_jwtToken.exp < currentTime) {
+    //handle logout
+    //window.location.href = "/" ;
+  }
+}
+
 class App extends Component {
   render() {
     return (
